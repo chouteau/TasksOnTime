2ec553cfdd93122a28608ec1271c2b5ec75ef9e6
Marc Chouteau <marc_chouteau@hotmail.com>
2022-02-09 14:48:41 +0100

    ++

--- src/DistributedTasksOnTime.BlazorComponent/ScheduledTaskList.razor.cs
+++ src/DistributedTasksOnTime.BlazorComponent/ScheduledTaskList.razor.cs
@@ -7,19 +7,26 @@ using System.Threading.Tasks;
 
 namespace DistributedTasksOnTime.BlazorComponent
 {
+	public class TaskInfo
+	{
+		public Orchestrator.Models.ScheduledTask ScheduledTask { get; set; }
+		public Orchestrator.Models.RunningTask LastRunningTask { get; set; }
+	}
+
 	public partial class ScheduledTaskList
 	{
 		[Inject] DistributedTasksOnTime.Orchestrator.ITasksOrchestrator TasksOrchestrator { get; set; }
 		[Inject] NavigationManager NavigationManager { get; set; }
 
-		IEnumerable<DistributedTasksOnTime.Orchestrator.Models.ScheduledTask> scheduledTaskList;
+		IEnumerable<TaskInfo> taskInfoList;
 
 		protected override void OnInitialized()
 		{
-			scheduledTaskList = TasksOrchestrator.GetScheduledTaskList();
+			taskInfoList = new List<TaskInfo>();
+			LoadTaskInfoList();
 			TasksOrchestrator.OnHostRegistered += async s =>
 			{
-				scheduledTaskList = TasksOrchestrator.GetScheduledTaskList();
+				LoadTaskInfoList();
 				await InvokeAsync(() =>
 				{
 					StateHasChanged();
@@ -27,12 +34,47 @@ namespace DistributedTasksOnTime.BlazorComponent
 			};
 			TasksOrchestrator.OnScheduledTaskStarted += async s =>
 			{
-				scheduledTaskList = TasksOrchestrator.GetScheduledTaskList();
+				LoadTaskInfoList();
 				await InvokeAsync(() =>
 				{
 					StateHasChanged();
 				});
 			};
+			TasksOrchestrator.OnRunningTaskChanged += async (s, r) =>
+			{
+				await InvokeAsync(() =>
+				{
+					LoadTaskInfoList();
+					var current = taskInfoList.FirstOrDefault(i => i.ScheduledTask.Name == r.TaskName);
+					if (current != null)
+					{
+						current.LastRunningTask = r;
+					}
+					StateHasChanged();
+				});
+			};
+
+		}
+
+		void LoadTaskInfoList()
+		{
+			var scheduledTaskList = TasksOrchestrator.GetScheduledTaskList();
+			foreach (var scheduledTask in scheduledTaskList)
+			{
+				var taskInfo = taskInfoList.FirstOrDefault(i => i.ScheduledTask.Name == scheduledTask.Name);
+				if (taskInfo != null)
+				{
+					taskInfo.ScheduledTask = scheduledTask;
+				}
+				else
+				{
+					((List<TaskInfo>)taskInfoList).Add(new TaskInfo
+					{
+						ScheduledTask = scheduledTask,
+						LastRunningTask = new()
+					});
+				}
+			}
 		}
 
 		void EditTask(Orchestrator.Models.ScheduledTask task)
